15cd4c7f66850973a999a056db66c5e498776a27
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-30 11:07:28 +0800

    initial commit

---
+++ src/main/java/com/example/BejC3S1SpringBootMcUserApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BejC3S1SpringBootMcUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BejC3S1SpringBootMcUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BejC3S1SpringBootMcUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejC3S1SpringBootMcUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
