87e66bcdafc996f750160af7052f91ab69589272
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-12-04 20:18:25 +0800

    created docker-compose.yml and DockerFile

--- src/main/java/com/example/BejC3S1SpringBootMcUserApplication.java
+++ src/main/java/com/example/BejC3S1SpringBootMcUserApplication.java
@@ -1,23 +1,13 @@
 package com.example;
 
-import com.example.filter.JwtFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BejC3S1SpringBootMcUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BejC3S1SpringBootMcUserApplication.class, args);
-	}
-
-	@Bean
-	public FilterRegistrationBean jwtFilter() {
-		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-		filterRegistrationBean.setFilter(new JwtFilter());
-		filterRegistrationBean.addUrlPatterns("/api/v1/*");
-		return filterRegistrationBean;
 	}
 }
--- src/main/java/com/example/service/SecurityTokenGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.domain.User;
-
-import java.util.Map;
-
-public interface SecurityTokenGenerator {
-    Map<String, String> generateToken(User user);
-}
